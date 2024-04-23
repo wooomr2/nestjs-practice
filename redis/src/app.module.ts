4f435d5b1b89fc0fc8ad64ab60dcb8ff5228d780
wooomr2 <wooomr2@gmail.com>
2024-04-23 21:32:48 +0900

    cache

--- redis/src/app.module.ts
+++ redis/src/app.module.ts
@@ -1,9 +1,11 @@
-import { CacheModule } from '@nestjs/cache-manager'
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
 import * as redisStore from 'cache-manager-redis-store'
 import { RedisClientOptions } from 'redis'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+
 @Module({
   imports: [
     CacheModule.register({
@@ -11,20 +13,22 @@ import { AppService } from './app.service'
     }),
 
     /** !! does not support redis v4:: downgraded to v3 will be fixed soon */
-    // CacheModule.register<RedisClientOptions>({
-    //   store: redisStore,
-    //   host: 'localhost',
-    //   port: 6379,
-    //   isGlobal: true,
-    // }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      socket: {
+        host: 'localhost',
+        port: 6379,
+      },
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [
     AppService,
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: CacheInterceptor,
-    // },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
